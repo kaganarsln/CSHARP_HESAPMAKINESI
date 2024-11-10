1f238919dd09bb71885161af31858d51c42a0a42
kaganarsln <kaan.arslangorurr@gmail.com>
2024-11-10 19:47:01 +0300

    CSHARP_HESAPMAKINESI

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.btn_hesapla = new System.Windows.Forms.Button();
+            this.lbl_sayi1 = new System.Windows.Forms.Label();
+            this.lbl_sayi2 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(123, 305);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(84, 20);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Toplama";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(249, 305);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(79, 20);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Çıkarma";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(361, 305);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(77, 20);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Çarpma";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(458, 305);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(68, 20);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Bölme";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(123, 349);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(67, 20);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "Karesi";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(249, 349);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(86, 20);
+            this.checkBox6.TabIndex = 5;
+            this.checkBox6.Text = "Karekökü";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // btn_hesapla
+            // 
+            this.btn_hesapla.Location = new System.Drawing.Point(347, 234);
+            this.btn_hesapla.Name = "btn_hesapla";
+            this.btn_hesapla.Size = new System.Drawing.Size(75, 23);
+            this.btn_hesapla.TabIndex = 6;
+            this.btn_hesapla.Text = "Hesapla";
+            this.btn_hesapla.UseVisualStyleBackColor = true;
+            // 
+            // lbl_sayi1
+            // 
+            this.lbl_sayi1.AutoSize = true;
+            this.lbl_sayi1.Location = new System.Drawing.Point(120, 215);
+            this.lbl_sayi1.Name = "lbl_sayi1";
+            this.lbl_sayi1.Size = new System.Drawing.Size(39, 16);
+            this.lbl_sayi1.TabIndex = 7;
+            this.lbl_sayi1.Text = "sayi1";
+            // 
+            // lbl_sayi2
+            // 
+            this.lbl_sayi2.AutoSize = true;
+            this.lbl_sayi2.Location = new System.Drawing.Point(120, 258);
+            this.lbl_sayi2.Name = "lbl_sayi2";
+            this.lbl_sayi2.Size = new System.Drawing.Size(39, 16);
+            this.lbl_sayi2.TabIndex = 8;
+            this.lbl_sayi2.Text = "sayi2";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(220, 208);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(220, 251);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 10;
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(481, 234);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(45, 16);
+            this.lbl3.TabIndex = 11;
+            this.lbl3.Text = "Sonuç";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(716, 439);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.lbl_sayi2);
+            this.Controls.Add(this.lbl_sayi1);
+            this.Controls.Add(this.btn_hesapla);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btn_giris;
+        private System.Windows.Forms.Button btn_iptal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.Button btn_hesapla;
+        private System.Windows.Forms.Label lbl_sayi1;
+        private System.Windows.Forms.Label lbl_sayi2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label lbl3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sayi1 = 0, sayi2 = 0, sonuc = 0;
+            sayi1 = double.Parse(textBox1.Text);
+            sayi2 = double.Parse(textBox2.Text);
+
+            if (checkBox1.Checked) 
+            {
+                sonuc = sayi1 + sayi2;
+            }
+            else if (checkBox2.Checked) 
+            {
+                sonuc = sayi1 - sayi2;
+            }
+            else if (checkBox3.Checked) 
+            {
+                sonuc = (sayi1 * sayi2);
+            }
+            else if (checkBox4.Checked) 
+            {
+                sonuc = (sayi1/sayi2);
+            }
+            else if (checkBox5.Checked) 
+            {
+                sonuc = Math.Pow(sayi1,sayi2);
+            }
+            else if (checkBox6.Checked)
+            {
+                sonuc = Math.Sqrt(sayi1);
+            }
+            lbl3.Text = sonuc.ToString();
+
+            MessageBox.Show(sonuc.ToString());
+        }
+    }
+}
+      
+
+
